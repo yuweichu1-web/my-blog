df9848defec97322a29d664979260123f4fe57d3
justinmy123 <yuweichu1@qq.com>
2026-02-10 12:11:54 +0800

    改造成汽车媒体博客：暗黑工业风主题

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// 极速车讯 - 汽车媒体博客配置
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '极速车讯';
+export const SITE_DESCRIPTION = '专注汽车资讯、新车评测、新能源动态、改装文化 - 探索汽车世界的每一个精彩瞬间';
